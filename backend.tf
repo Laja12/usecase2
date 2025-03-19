8babd310734a1e0a34b63f3b2aefc68ebd79d897
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-03-19 11:42:23 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "bucket-for-threetier"  # Replace with your unique S3 bucket name
+    bucket = "loadbalance1-project-bucket"  # Replace with your unique S3 bucket name
     key    = "terraform/key/terraform.tfstate"  # Path inside the bucket to store the state
+    region = "ap-southeast-2"
     encrypt = true  # Enable encryption of the state file
    
   }
