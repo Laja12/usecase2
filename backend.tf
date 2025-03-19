8de8f4e625f4bea6c41fcf223d34d21856cf5fe0
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-03-19 16:50:41 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "loadbalance1-project-bucket"  # Replace with your unique S3 bucket name
-    key    = "terraform/key/terraform.tfstate"  # Path inside the bucket to store the state
+    key    = "files/key/terraform.tfstate"  # Path inside the bucket to store the state
     region = "ap-southeast-2"
     encrypt = true  # Enable encryption of the state file
     dynamodb_table = "terraform-locks"
