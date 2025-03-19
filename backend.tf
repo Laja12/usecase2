2f446548543f43ee71fa43a35e670e344da2d895
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-03-19 16:40:44 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,9 @@ terraform {
     key    = "terraform/key/terraform.tfstate"  # Path inside the bucket to store the state
     region = "ap-southeast-2"
     encrypt = true  # Enable encryption of the state file
+
+   dynamodb_table = "terraform-locks"
+   acl = "bucket-owner-full-control"
    
   }
 }
